cfd7027a43368b2e39231b27f7ebc6aeb662b263
Ivan Iliev <ilie0012@hz.nl>
2024-11-27 16:38:30 +0100

    reorganize achievements components and add the ability to override the backRoute

--- src/routes/achievements/+page.svelte
+++ src/routes/achievements/+page.svelte
@@ -1,19 +1,14 @@
 <script>
     import Header from "$lib/components/layout/Header.svelte";
-    import AchievementsList from "$lib/components/ui/AchievementsList.svelte";
+    import AchievementsList from "$lib/components/achievements/AchievementsList.svelte";
     import PageContainer from "$lib/components/layout/PageContainer.svelte";
 
     import { goto } from "$app/navigation";
-
-    // Back button handler
-    const handleBack = () => {
-        goto("/profile"); // Redirect to the homepage or another route
-    };
 </script>
 
 <PageContainer>
     <!-- Page Header -->
-    <Header title="Achievements" showBack={true} />
+    <Header title="Achievements" showBack={true} backRoute="/profile"/>
 
     <!-- Achievements List -->
     <AchievementsList />
